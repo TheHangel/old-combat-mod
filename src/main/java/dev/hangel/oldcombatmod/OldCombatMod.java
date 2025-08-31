58ddc846a546d731107a412a3e4ab6dc52b178fa
Hangel <thibaud.lebrasseur@etu.unicaen.fr>
2025-08-31 21:03:36 +0200

    1.21.1 Neoforge

--- src/main/java/dev/hangel/oldcombatmod/OldCombatMod.java
+++ src/main/java/dev/hangel/oldcombatmod/OldCombatMod.java
@@ -3,21 +3,17 @@ package dev.hangel.oldcombatmod;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.attributes.RangedAttribute;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.GameRules;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.tick.ServerTickEvent;
-import org.slf4j.Logger;
-import com.mojang.logging.LogUtils;
 import net.neoforged.fml.common.Mod;
 
 @Mod(OldCombatMod.MODID)
 public class OldCombatMod
 {
     public static final String MODID = "old_combat_mod";
-    private static final Logger LOGGER = LogUtils.getLogger();
 
     public static final GameRules.Key<GameRules.BooleanValue> OLD_COMBAT =
             GameRules.register("oldCombat",
