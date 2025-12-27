6c5d1d87a8a0287f74b83a160184338c8993e92f
Hangel <thibaud.lebrasseur@etu.unicaen.fr>
2025-12-27 17:24:45 +0100

    feat(Multi loader): Refractor example mod to old combat mod

---
+++ common/src/main/java/dev/hangel/old_combat_mod/Constants.java
@@ -0,0 +1,10 @@
+package dev.hangel.old_combat_mod;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+	public static final String MOD_ID = "old_combat_mod";
+	public static final String MOD_NAME = "Old Combat Mod";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ fabric/src/main/java/dev/hangel/old_combat_mod/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package dev.hangel.old_combat_mod.platform;
+
+import dev.hangel.old_combat_mod.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
