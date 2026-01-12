2ed06f22c3aa5a0c62d6190e1da75e3423f9b30b
Hangel <105165180+TheHangel@users.noreply.github.com>
2026-01-12 22:52:17 +0100

    fix(Forge): run bug

---
+++ forge/src/main/java/dev/hangel/old_combat_mod/mixin/PlayerMixin.java
@@ -0,0 +1,24 @@
+package dev.hangel.old_combat_mod.mixin;
+
+import dev.hangel.old_combat_mod.gamerules.OldCombatGamerule;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(Player.class)
+public class PlayerMixin {
+    @Redirect(
+            method = "causeExtraKnockback(Lnet/minecraft/world/entity/Entity;FLnet/minecraft/world/phys/Vec3;)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;setSprinting(Z)V")
+    )
+    private void oldCombat$dontStopSprintingOnKnockback(Player self, boolean sprinting) {
+        if (self.level() instanceof ServerLevel serverLevel) {
+            boolean enabled = serverLevel.getGameRules().get(OldCombatGamerule.OLD_COMBAT);
+            if (enabled) return;
+        }
+        self.setSprinting(sprinting);
+    }
+}
+
