06f49beb056664236d21409c455f57248015a003
Hangel <105165180+TheHangel@users.noreply.github.com>
2026-04-03 22:27:27 +0200

    Forge : common java

--- forge/src/main/java/dev/hangel/old_combat_mod/OldCombatMod.java
+++ forge/src/main/java/dev/hangel/old_combat_mod/OldCombatMod.java
@@ -1,18 +1,29 @@
 package dev.hangel.old_combat_mod;
 
 import dev.hangel.old_combat_mod.gamerules.OldCombatGamerule;
+import net.minecraft.core.registries.Registries;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.RegisterEvent;
 
-@Mod("old_combat_mod")
+@Mod(Constants.MOD_ID)
 public class OldCombatMod {
 
-    public OldCombatMod() {
-        OldCombatGamerule.register();
+    public OldCombatMod(FMLJavaModLoadingContext context) {
+        CommonClass.init();
+
+        RegisterEvent.getBus(context.getModBusGroup()).addListener(this::onRegister);
 
         TickEvent.LevelTickEvent.Post.BUS.addListener(OldCombatMod::onLevelTick);
         PlayerEvent.PlayerLoggedInEvent.BUS.addListener(OldCombatMod::onPlayerLogin);
+    }
+
+    private void onRegister(RegisterEvent event) {
+        if (event.getRegistryKey().equals(Registries.GAME_RULE)) {
+            OldCombatGamerule.register();
+        }
     }
 
     private static void onLevelTick(TickEvent.LevelTickEvent.Post event) {
