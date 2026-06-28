928f85752b88205dbacf7793b4f8e06098d92b36
Hangel <105165180+TheHangel@users.noreply.github.com>
2026-06-28 20:24:29 +0200

    26.2
    * Fabric & NeoForge updated (from https://githubcom/jaredlll08/MultiLoader-Template)
    
    * Forge : import examplemod
    
    * Forge : working v1
    
    * Forge : common java
    
    * Optimization : remove duplicated code
    
    * Ci/Cd : optimization rm duplicated code + forge
    
    * Ci/Cd : read version as an action
    
    * feat(26.2): update
    
    * fix(26.2): cleaning
    
    * fix(26.2): mixin
    
    * fix(26.2): fabric gamerule
    
    * fix(26.2): fabric logic rm

--- common/src/main/java/dev/hangel/old_combat_mod/platform/Services.java
+++ common/src/main/java/dev/hangel/old_combat_mod/platform/Services.java
@@ -9,11 +9,8 @@ public class Services {
     public static final IPlatformHelper PLATFORM = load(IPlatformHelper.class);
 
     public static <T> T load(Class<T> clazz) {
-
-        final T loadedService = ServiceLoader.load(clazz)
+        return ServiceLoader.load(clazz)
                 .findFirst()
                 .orElseThrow(() -> new NullPointerException("Failed to load service for " + clazz.getName()));
-        Constants.LOG.debug("Loaded {} for service {}", loadedService, clazz);
-        return loadedService;
     }
 }
--- fabric/src/main/java/dev/hangel/old_combat_mod/OldCombatMod.java
+++ fabric/src/main/java/dev/hangel/old_combat_mod/OldCombatMod.java
@@ -1,32 +1,30 @@
 package dev.hangel.old_combat_mod;
 
+import dev.hangel.old_combat_mod.gamerules.OldCombatGamerule;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleBuilder;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.minecraft.resources.Identifier;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.level.gamerules.GameRule;
 import net.minecraft.world.level.gamerules.GameRuleCategory;
 
 public class OldCombatMod implements ModInitializer {
 
-    public static GameRule<Boolean> OLD_COMBAT;
-
     @Override
     public void onInitialize() {
-        OLD_COMBAT = GameRuleBuilder
+        OldCombatGamerule.OLD_COMBAT = GameRuleBuilder
                 .forBoolean(true)
                 .category(GameRuleCategory.PLAYER)
                 .buildAndRegister(Identifier.fromNamespaceAndPath(Identifier.DEFAULT_NAMESPACE, "old_combat"));
 
         ServerTickEvents.END_LEVEL_TICK.register(world -> {
             if (world instanceof ServerLevel serverLevel) {
-                OldCombatLogicFabric.onWorldTick(serverLevel);
+                OldCombatLogic.onWorldTick(serverLevel);
             }
         });
 
         ServerPlayConnectionEvents.JOIN.register((handler, sender, server) ->
-                OldCombatLogicFabric.onPlayerJoin(handler.player));
+                OldCombatLogic.onPlayerJoin(handler.player));
     }
 }
