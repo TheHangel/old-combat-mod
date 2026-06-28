d2b39eca62f5825946f25a32cc2477d8a9e8a7e5
Hangel <105165180+TheHangel@users.noreply.github.com>
2026-06-28 19:59:28 +0200

    fix(26.2): fabric gamerule

--- fabric/src/main/java/dev/hangel/old_combat_mod/OldCombatMod.java
+++ fabric/src/main/java/dev/hangel/old_combat_mod/OldCombatMod.java
@@ -3,14 +3,20 @@ package dev.hangel.old_combat_mod;
 import dev.hangel.old_combat_mod.gamerules.OldCombatGamerule;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleBuilder;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.resources.Identifier;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.gamerules.GameRuleCategory;
 
 public class OldCombatMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        OldCombatGamerule.register();
+        OldCombatGamerule.OLD_COMBAT = GameRuleBuilder
+                .forBoolean(true)
+                .category(GameRuleCategory.PLAYER)
+                .buildAndRegister(Identifier.fromNamespaceAndPath(Identifier.DEFAULT_NAMESPACE, "old_combat"));
 
         ServerTickEvents.END_LEVEL_TICK.register(world -> {
             if (world instanceof ServerLevel serverLevel) {
